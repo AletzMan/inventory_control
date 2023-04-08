cf96b86941386b068b3b8ea2d5a17f45d5138ca6
= <=>
2023-04-08 00:56:50 -0600

    Created sections and menu sections

--- Inventory_Control/MainWindow.xaml.cs
+++ Inventory_Control/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Inventory_Control.pages;
 
 namespace Inventory_Control
 {
@@ -20,19 +21,107 @@ namespace Inventory_Control
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            LoadDashboard();
+            //ResetBackgroundButtons();
+            //Button Children = NavigationMenu.Children[2] as Button;
+
         }
 
-        private void minimizeButton_Click(object sender, RoutedEventArgs e)
+        private void LoadDashboard()
         {
+            viewPage.NavigationService.Navigate(new DashboardPage());
+        }
+
+        private void ResetBackgroundButtons(Button sender)
+        {
+            int ChildrenCount = NavigationMenu.Children.Count;
+            for (int i = 1; i < ChildrenCount; i++)
+            {
+                Button menuItem = NavigationMenu.Children[i] as Button;
+                menuItem.Background = (Brush)Application.Current.Resources["TertiaryBrush"];
+            }
+            Button button = sender as Button;
+            button.Background = (Brush)Application.Current.Resources["PrimaryBrush"];
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void DashboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewPage.NavigationService.Navigate(new DashboardPage());
+            Console.WriteLine(NavigationMenu.Children);
+            ResetBackgroundButtons(sender as Button);
 
         }
 
-        private void closeButton_Click(object sender, RoutedEventArgs e)
+        private void PermissionButton_Click(object sender, RoutedEventArgs e)
         {
+            viewPage.NavigationService.Navigate(new PermissionsPage());
+            //Button button = sender as Button;
+            //button.Background = (Brush)Application.Current.Resources["PrimaryBrush"];
+            ResetBackgroundButtons(sender as Button);
+        }
 
+        private void CategoriesButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewPage.NavigationService.Navigate(new CategoriesPage());
+            ResetBackgroundButtons(sender as Button);
+        }
+
+        private void SupplierButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewPage.NavigationService.Navigate(new SuppliersPage());
+            ResetBackgroundButtons(sender as Button);
+        }
+
+        private void SalesButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewPage.NavigationService.Navigate(new SalesPage());
+            ResetBackgroundButtons(sender as Button);
+        }
+
+        private void InventoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewPage.NavigationService.Navigate(new InventoryPage());
+            ResetBackgroundButtons(sender as Button);
+        }
+
+        private void ReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewPage.NavigationService.Navigate(new ReportPage());
+            ResetBackgroundButtons(sender as Button);
+        }
+
+        private void MenuButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            button.Opacity = 0.7;
+        }
+        private void MenuButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            button.Opacity = 1;
         }
     }
 }
