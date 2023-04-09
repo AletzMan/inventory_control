a96f9f3af0f79d68fdc83d06d95587ec90c6b825
= <=>
2023-04-08 23:06:33 -0600

    Terminated dashboard layout page

--- Inventory_Control/pages/DashboardPage.xaml.cs
+++ Inventory_Control/pages/DashboardPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,43 @@ namespace Inventory_Control.pages
     /// </summary>
     public partial class DashboardPage : Page
     {
+        public SeriesCollection SeriesCollection { get; set; }
+        public SeriesCollection WeeklySales { get; set; }
+        public string[] Labels { get; set; }
+        public string[] WeeklyLabels { get; set; }
+        public Func<double, string> Formatter { get; set; }
         public DashboardPage()
         {
             InitializeComponent();
+
+            WeeklySales = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Abril 2 - Abril 8",
+                    Values = new ChartValues<double> { 10000, 7000, 8000, 9500, 5000, 8500, 12000 }
+                }
+            };
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "2023",
+                    Values = new ChartValues<double> { 120000, 130000, 150000, 45000 }
+                }
+            };
+
+
+            DataContext = this;
+            Labels = new[] { "Enero", "Feberero", "Marzo", "Abril" };
+            WeeklyLabels = new[] { "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado" };
+            Formatter = value => value.ToString("C", CultureInfo.CreateSpecificCulture("es-MX"));
+
+            
+
         }
+
+      
     }
+
 }
