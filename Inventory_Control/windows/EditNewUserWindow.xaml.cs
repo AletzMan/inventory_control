34f862de843fd88dc6f6b66421245fc0df541643
= <=>
2023-04-10 14:02:30 -0600

    Added email validation and username validation only letter and spaces in permissions page

--- Inventory_Control/windows/EditNewUserWindow.xaml.cs
+++ Inventory_Control/windows/EditNewUserWindow.xaml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -57,7 +58,7 @@ namespace Inventory_Control.windows
 
         private void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
-            if (UserID == 0)
+            if (ValidateFields())
             {
                 User newUser = new User
                 {
@@ -65,19 +66,90 @@ namespace Inventory_Control.windows
                     Email = userEmail.Text,
                     Permission = userPermission.Text,
                 };
-                EditQuery.AddUser(newUser);
-            }
-            else
-            {
-                User newUser = new User
+                if (UserID == 0)
                 {
-                    Name = userName.Text,
-                    Email = userEmail.Text,
-                    Permission = userPermission.Text,
-                };
-                EditQuery.EditUser(UserID, newUser);
+                    EditQuery.AddUser(newUser);
+                }
+                else
+                {
+                    EditQuery.EditUser(UserID, newUser);
+                }
+                Close();
             }
-            Close();
+        }
+
+        private bool ValidateFields()
+        {
+            string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov|mx|dev)$";
+            if (userName.Text == string.Empty)
+            {
+                userName.BorderBrush = Brushes.Red;
+                errorUsername.Visibility = Visibility.Visible;
+                errorUsername.Content = "No puede quedar vacío";
+                return false;
+            }
+            if (userEmail.Text == string.Empty)
+            {
+                userEmail.BorderBrush = Brushes.Red;
+                errorEmail.Visibility = Visibility.Visible;
+                errorEmail.Content = "No puede quedar vacío";
+                return false;
+            }
+            if (userPermission.Text == string.Empty)
+            {
+                userPermission.BorderBrush = Brushes.Red;
+                errorPermission.Visibility = Visibility.Visible;
+                return false;
+            }
+            if (userName.Text.Length < 4)
+            {
+                userName.BorderBrush = Brushes.Red;
+                errorUsername.Visibility = Visibility.Visible;
+                errorUsername.Content = "Elija un nombre de al menos 4 caracteres";
+                return false;
+            }
+            if (!Regex.IsMatch(userName.Text, @"^[a-zA-Z\s]+$"))
+            {
+                userName.BorderBrush = Brushes.Red;
+                errorUsername.Visibility = Visibility.Visible;
+                errorUsername.Content = "Elija sólo letras";
+                return false;
+            }
+            if (!Regex.IsMatch(userEmail.Text, regex, RegexOptions.IgnoreCase))
+            {
+                errorEmail.BorderBrush = Brushes.Red;
+                errorEmail.Visibility = Visibility.Visible;
+                errorEmail.Content = "Formato de correo no válido";
+                return false;
+            }
+            return true;
+        }
+
+        private void UserName_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+            var typeObject = sender.GetType();
+            if (typeObject.Name == "TextBox")
+            {
+                TextBox textBoxField = sender as TextBox;
+                if (textBoxField.Name == "userName")
+                {
+                    errorUsername.Visibility = Visibility.Hidden;
+                }
+                if (textBoxField.Name == "userEmail")
+                {
+                    errorEmail.Visibility = Visibility.Hidden;
+                }
+                textBoxField.BorderBrush = (Brush)Application.Current.Resources["OptionalBrush"];
+            }
+            else if (typeObject.Name == "ComboBox")
+            {
+                ComboBox comboBoxField = sender as ComboBox;
+                comboBoxField.BorderBrush = (Brush)Application.Current.Resources["OptionalBrush"];
+                errorPermission.Visibility = Visibility.Hidden;
+            }
+
+
         }
     }
 }
